f97b44f35f8e14464d98b9d5355fd6f26ed7cfa7
David Boyne <boyneyy123@gmail.com>
2025-04-16 15:35:22 +0100

    feat(plugin): using annotations for mappings

--- src/components/DocumentationEntity/index.tsx
+++ src/components/DocumentationEntity/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
+import { useApi, configApiRef, ConfigApi } from '@backstage/core-plugin-api';
 import { useEntity } from '@backstage/plugin-catalog-react';
-import { useApi, configApiRef } from '@backstage/core-plugin-api';
 import { Entity } from '@backstage/catalog-model';
-import {} from '../..';
-import { EventCatalogConfig, getConfig, ResourceMap } from '../../config';
 
 interface Props {
   page: 'docs' | 'visualiser' | 'discover';
@@ -25,104 +23,70 @@ const getEventCatalogCollectionFromEntity = (entity: Entity) => {
     return 'services';
   }
 
-  if(isEntityDomain(entity)) {
+  if (isEntityDomain(entity)) {
     return 'domains';
   }
 
   return null;
 };
 
-const getEventCatalogValuesFromEntity = (entity: Entity, map: ResourceMap[]) => {
+export function getConfig(config: ConfigApi) {
+  const pluginConfig = config.getConfig('eventcatalog');
 
-    const data = map.find(
-        item => item['backstage-name'] === entity.metadata.name,
-    );
-    
-    if (!data) return null;
-
-    let version = data['eventcatalog-version'] || '';
-    version = version === 'latest' ? '' : version;
-    
-    return { id: data['eventcatalog-id'], version, discoverFilter: data['eventcatalog-page-discovery-default-filter'] };
-
+  return {
+    URL: pluginConfig.getString('URL')
+  };
 }
 
-const backstageToCatalogResourceName = (
-  entity: Entity,
-  pluginConfig: EventCatalogConfig,
-): { id: string; version?: string, discoverFilter?: string  } | null => {
-  const { services = [], apis = [] } = pluginConfig;
+export const EventCatalogDocumentationEntityPage = (props: Props) => {
+  const { page = 'docs' } = props;
+  const resource = useEntity();
 
-  if (isEntityService(entity)) {
-    return getEventCatalogValuesFromEntity(entity, [...services, ...apis]);
-  }
-
-  return null;
-};
-
-export const EventCatalogDocumentationEntityPage = ({
-  page = 'docs',
-}: Props) => {
-  const { entity } = useEntity();
   const config = useApi(configApiRef);
   const pluginConfig = getConfig(config);
 
-  const collection = getEventCatalogCollectionFromEntity(entity);
+  const eventCatalogResourceId = resource.entity.metadata.annotations?.['eventcatalog.dev/id'] || null;
+  const eventCatalogResourceVersion = resource.entity.metadata.annotations?.['eventcatalog.dev/version'] || null;
+  const eventCatalogResourceCollection = resource.entity.metadata.annotations?.['eventcatalog.dev/collection'] || null;
 
-  if (!collection) {
+  const collection = eventCatalogResourceCollection || getEventCatalogCollectionFromEntity(resource.entity);
+
+  if (!eventCatalogResourceId) {
     return (
       <div style={{ fontStyle: 'italic', opacity: '0.5' }}>
         <span style={{ display: 'block' }}>
-          This entity kind "{entity.kind}" is not supported by the EventCatalog
-          plugin.{' '}
-        </span>
-        <span style={{ display: 'block' }}>
-          Please raise an issue on the plugin if you would like to support it.{' '}
-        </span>
-      </div>
-    );
-  }
-
-  // Try and see if there is mapping for the entity name to EventCatalog
-  const eventCatalogResource = backstageToCatalogResourceName(
-    entity,
-    pluginConfig,
-  );
-
-  if (!eventCatalogResource) {
-    return (
-      <div style={{ fontStyle: 'italic', opacity: '0.5' }}>
-        <span style={{ display: 'block' }}>
-          Cannot find a mapping for this entity ({entity.metadata.name}) in
-          EventCatalog.
-        </span>
-        <span style={{ display: 'block' }}>
-          Please provide a <a style={{ textDecoration: "underline", color: "#fff"}} target="_blank" href="https://www.eventcatalog.dev/docs/development/plugins/backstage/api">mapping from the entity name to the EventCatalog ID </a>
-          in the plugin config.
+          Cannot find a mapping for this entity ({resource.entity.metadata.name}) in EventCatalog.
+          Please use annotation eventcatalog.dev/id to map the entity to an EventCatalog resource.
         </span>
       </div>
     );
   }
 
   let url = new URL(
-    `/${page}/${collection}/${eventCatalogResource.id}/${eventCatalogResource.version}?embed=true`,
+    `/${page}/${collection}/${eventCatalogResourceId}?embed=true`,
     pluginConfig.URL,
-  ).toString();
+  );
+
+  if(eventCatalogResourceVersion) {
+    url = new URL(
+      `/${page}/${collection}/${eventCatalogResourceId}/${eventCatalogResourceVersion}?embed=true`,
+      pluginConfig.URL,
+    );
+  };
 
   if(page === 'discover') {
-    const filter = eventCatalogResource.discoverFilter || eventCatalogResource.id;
     url = new URL(
-      `/${page}/${collection}?embed=true&id=${filter}`,
+      `/${page}/${collection}?embed=true`,
       pluginConfig.URL,
-    ).toString();
+    );
   }
 
   return (
     <div style={{ background: 'white', height: '100%' }}>
-      <iframe title={url} src={url} width="100%" height="100%" />
+      <iframe title={url.toString()} src={url.toString()} width="100%" height="100%" />
     </div>
   );
-};
+}
 
 export const EventCatalogEntityVisualiserCard = () => {
   return (<div style={{ height: '100%'}}>
@@ -134,4 +98,3 @@ export const EventCatalogEntityMessageCard = () => {
     <EventCatalogDocumentationEntityPage page="discover" />
   </div>);
 };
-
